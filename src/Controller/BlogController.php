3fe1b6cf4191df0e367d9095a4a48c9ba1f502fa
Lova Ramih <lova.ramiharisoa@gmail.com>
2024-01-19 23:15:13 +0300

    Création du formulaire classique et configuration des navigation

--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -2,7 +2,11 @@
 
 namespace App\Controller;
 
+use App\Entity\Article;
+use App\Repository\ArticleRepository;
+use Doctrine\Persistence\ObjectManager;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -20,19 +24,42 @@ class BlogController extends AbstractController
     /**
      * @Route("/",name="home")
      */
-    public function home()
+    public function home(ArticleRepository $repo)
     {
+        //$repo = $this->getDoctrine()->getRepository(Article::class);
+        $artilces = $repo->findAll();
         return $this->render('blog/home.html.twig', [
-            'title' => 'Mon Blog'
+            'title' => 'Mon Blog',
+            'articles' => $artilces
         ]);
     }
     /**
-     * @Route("/show",name="showArticle")
+     * @Route("/blog/create",name="createArticle")
      */
-    public function showArticle()
+    public function create(Request $request)
     {
+        dump($request);
+        if ($request->request->count() > 0) {
+            $article = new Article();
+            $article -> setContent($request->request->get('nom'))
+                     -> setTitle($request->request->get('prenom'))
+                     -> setCreatedAt(new \DateTime());
+            $manager = $this->getDoctrine()->getManager();
+            $manager->persist($article);
+            $manager->flush();   
+            return $this->redirectToRoute('showArticle', ['id'=>$article->getId()]);
+        }
+        return $this->render('blog/create.html.twig');
+    }
+    /**
+     * @Route("/blog/{id}",name="showArticle")
+     */
+    public function showArticle(ArticleRepository $repo,$id)
+    {
+        //$repo = $this->getDoctrine()->getRepository(Article::class);
+        $article = $repo->find($id);
          return $this->render('blog/showArticle.html.twig',[
-            
+            'article' => $article
         ]);
     }
 }
